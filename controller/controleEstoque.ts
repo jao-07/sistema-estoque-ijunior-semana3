2021f9564d5d3051a4215f42cc21dd8c284e8801
Joao Victor Evangelista Cruz <joaovecruz@yahoo.com.br>
2025-07-20 23:19:31 -0300

    refactor(estoque): Altera os arquivos para que as regras de negócio fiquem no service, o controller só chame as funções de service e o index só inicialize o programa

--- controller/controleEstoque.ts
+++ controller/controleEstoque.ts
@@ -1,34 +1,76 @@
-import { Data } from "../model/interfaceData.js";
-import { estoqueService } from "../service/serviceEstoque.js";
+import { Data } from "../model/interfaceData.js"
+import { estoqueService } from "../service/serviceEstoque.js"
+import promptSync from 'prompt-sync'
 
-const service = new estoqueService
+export default class estoqueController{
+    service: estoqueService
+    rodando: boolean
+    prompt: any
 
-export const adicionarProduto = async (data: Data) => {
-    try{
-        await service.criar(data)
-        console.log("Produto adicionado com sucesso.")
+    constructor(){
+        this.service = new estoqueService
+        this.rodando = true
+        this.prompt = promptSync()
     }
-    catch(error){
-        console.error("Erro ao adicionar o produto: ", error)
-    }
-}
 
-export const listarProdutos = async () => {
-    try {
-        const produtos = service.listar()
-        return produtos
+    adicionarProduto = async () => {
+        try{
+            await this.service.criar()
+            console.log("Produto adicionado com sucesso.")
+        }
+        catch(error){
+            console.error("Erro ao adicionar o produto: ", error)
+        }
     }
-    catch(error){
-        console.error("Erro ao listar os produtos", error)
-    }
-}
 
-export const removerProduto = async (nome: string) => {
-    try {
-        await service.remover(nome)
-        console.log("Produto removido com sucesso!")
+    listarProdutos = async () => {
+        try {
+            const produtos = this.service.listar()
+            return produtos
+        }
+        catch(error){
+            console.error("Erro ao listar os produtos", error)
+        }
     }
-    catch(error){
-        console.error("Erro ao remover o produto!", error)
+
+    removerProduto = async (nome: string) => {
+        try {
+            await this.service.remover(nome)
+            console.log("Produto removido com sucesso!")
+        }
+        catch(error){
+            console.error("Erro ao remover o produto!", error)
+        }
+    }
+
+    main = async () => {
+        while(this.rodando){
+            console.log("\n\nBem vindo ao sistema de estoque \nUtilize os números para selecionar alguma das opções abaixo:")
+            console.log(" 1 - Adicionar item \n 2 - Remover item \n 3 - Listar itens \n 4 - Mostrar valor total do estoque \n 5 - Mostrar peso total do estoque \n 6- Calcular média de valor dos produtos \n 7 - Calcular média de peso dos itens \n 8- Mostrar quantidade total de itens \n 9 - Mostrar quantidade total de produtos \n 0 - Sair\n\n")
+
+            let escolha = parseInt(this.prompt('Selecione uma opção: '), 10)
+
+            if(escolha == 0){
+                this.rodando = false
+            }
+
+            if(escolha == 1){
+                await this.adicionarProduto()
+            }
+
+            if(escolha == 2){
+                let produtoAlvo = this.prompt('Digite o nome do produto a ser removido: ')
+                await this.removerProduto(produtoAlvo)
+            }
+
+            if(escolha == 3){
+                let lista = await this.listarProdutos()
+                if(lista?.length == 0)
+                    console.log("Lista vazia")
+                lista?.forEach(element => {
+                    console.log(`Nome: ${element.nome} | Valor: ${element.valor} | Peso: ${element.peso} | Quantidade: ${element.quantidade}`)
+                });
+            }
+        }
     }
 }
--- service/serviceEstoque.ts
+++ service/serviceEstoque.ts
@@ -1,12 +1,61 @@
 import { readCSV } from "../model/readCSV.js";
 import { writeCSV } from "../model/writeCSV.js";
 import { Data } from "../model/interfaceData.js";
-import fs from 'fs'
+import promptSync from 'prompt-sync'
 
 const filePath = 'db/estoque.csv'
 
 export class estoqueService{
-    async criar(data: Data){
+    prompt: any
+
+    constructor(){
+        this.prompt = promptSync()
+    }
+
+    selecionarDadosDoProduto = () => {
+        let produto: Data = {nome: "", valor: -1, quantidade: -1, peso: -1}
+        while(produto.nome === ""){
+            let nome = this.prompt('Digite o nome do produto: ')
+            if(nome?.length == 0){
+                console.log("Nome inválido!")
+                continue
+            }
+            produto.nome = nome
+        }
+
+        while(produto.valor === -1){
+            let valor = parseFloat(this.prompt('Digite o valor do item: '))
+            if(valor < 0 || isNaN(valor)){
+                console.log("Valor inválido!")
+                continue
+            }
+            produto.valor = valor
+        }
+
+        while(produto.peso === -1){
+            let peso = parseFloat(this.prompt('Digite o peso do item: '))
+            if(peso < 0 || isNaN(peso)){
+                console.log("Peso inválido!")
+                continue
+            }
+            produto.peso = peso
+        }
+
+        while(produto.quantidade === -1 ){
+            let quantidade = parseInt(this.prompt('Digite a quantidade do item: '),10)
+            if(quantidade < 0 || isNaN(quantidade)){
+                console.log("Quantidade inválida!")
+                continue
+            }
+            produto.quantidade = quantidade
+        }
+
+        return produto
+    }
+
+    async criar(){
+        const data = this.selecionarDadosDoProduto()
+
         if(typeof data.nome !== 'string' || data.nome.length == 0)
             throw new Error('Campo nome inválido!')
 
